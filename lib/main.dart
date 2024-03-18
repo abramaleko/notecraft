5be84e32670834ee1ec8d363fd4be7599ac00f3d
Abraham Maleko <abrahammaleko@gmail.com>
2024-03-18 12:33:40 +0300

    added and configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:notecraft/firebase_options.dart';
 import 'package:notecraft/routes/app_routes.dart';
 import 'package:notecraft/views/notes.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
